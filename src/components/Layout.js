101cc2acd5b5231b771dd36536099691984b52d9
dccoleman <dccoleman@wpi.edu>
2019-05-13 21:31:29 -0400

    add change to title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ export default ({ children, title = "Blog" }) => {
             <HomeIcon />
           </IconButton>
           <Typography variant="h5" gutterBottom>
-            {title}
+            TEST: {title}
           </Typography>
           <IconButton>
             <CloseIcon />
