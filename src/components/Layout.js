b103ec212303984d099ded9fbd8ba5e5541fe8f9
dccoleman <dccoleman@wpi.edu>
2019-05-18 09:21:16 -0400

    testing changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ export default ({ children, title = "Blog" }) => {
             <HomeIcon />
           </IconButton>
           <Typography variant="h5" gutterBottom>
-            HI JULIE: {title}
+            HI AGAIN JULIE: {title}
           </Typography>
           <IconButton>
             <CloseIcon />
